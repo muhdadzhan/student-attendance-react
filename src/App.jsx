7d6f016602dc11e818eecd81d8b69fe5e385a917
AdzhanRE <muhdadzhan@gmail.com>
2025-08-05 23:37:51 +0800

    Add Login and Route Function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
 
@@ -9,9 +11,17 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route 
+            path='/login'
+            element={<Login/>}
+          />
           <Route
             path='/'
-            element={<Dashboard/>}
+            element={
+              <ProtectedRoute>
+                <Dashboard/>
+              </ProtectedRoute>
+            }
           />
         </Routes>
       </Router>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import API from "../api";
+import { useNavigate } from "react-router-dom";
+
+
+function Login(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try{
+            const res = await API.post('/login', {email, password});
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('role', res.data.role);
+            navigate('/');
+        }
+        catch(err){
+            alert('Login Failed');
+        }
+    };
+
+    return(
+        <div className="container mt-5">
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} className="form-control mb-2" placeholder="Email" />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control mb-2" placeholder="Password" />
+                <button className="btn btn-primary">Login</button>
+            </form>
+        </div>
+    );
+
+}
+
+export default Login;
