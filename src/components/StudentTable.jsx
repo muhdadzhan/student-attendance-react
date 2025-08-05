afbdd771f29e9d821da319996ae93b3bb5120be5
AdzhanRE <muhdadzhan@gmail.com>
2025-08-05 15:33:01 +0800

    Initialize Commit

---
+++ src/components/StudentTable.jsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import API from '../api';
+
+
+function StudentTable(){
+
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        API.get('/students')
+            .then((res) => setStudents(res.data))
+            .catch((err) => console.error(err));
+    }, []);
+
+
+    return(
+        <div className="container mt-4">
+            <h2>Student List</h2>
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th><th>Name</th><th>Class</th>
+                </tr>
+                </thead>
+                <tbody>
+                {students.map((s) => (
+                    <tr key={s.id}>
+                    <td>{s.id}</td>
+                    <td>{s.name}</td>
+                    <td>{s.class}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+
+}
+
+export default StudentTable;
